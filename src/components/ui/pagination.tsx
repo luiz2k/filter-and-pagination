5488af568b402da1bef82e029b37cb9c5c07dacd
luiz2k <luizteles934@gmail.com>
2025-05-18 15:09:00 -0300

    feat: adicionar props e funcionalidade de disabled em PaginationLink

--- src/components/ui/pagination.tsx
+++ src/components/ui/pagination.tsx
@@ -39,12 +39,14 @@ function PaginationItem({ ...props }: React.ComponentProps<"li">) {
 
 type PaginationLinkProps = {
   isActive?: boolean;
+  disabled?: boolean;
 } & Pick<React.ComponentProps<typeof Button>, "size"> &
   React.ComponentProps<"a">;
 
 function PaginationLink({
   className,
   isActive,
+  disabled,
   size = "icon",
   ...props
 }: PaginationLinkProps) {
@@ -58,6 +60,7 @@ function PaginationLink({
           variant: isActive ? "outline" : "ghost",
           size,
         }),
+        disabled && "pointer-events-none opacity-50",
         className,
       )}
       {...props}
