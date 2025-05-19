d0a6bb5d69de3e3ad168420f68aa17d882092153
luiz2k <luizteles934@gmail.com>
2025-05-19 13:20:14 -0300

    feat: adicionar constante que traduz as chaves refentes a tabela Task

---
+++ src/constants/order-by-translate.ts
@@ -0,0 +1,9 @@
+import { TaskKeys } from "@/types/task-keys";
+
+export const ORDER_BY_TRANSLATE: Record<TaskKeys, string> = {
+  title: "Título",
+  description: "Descrição",
+  status: "Status",
+  createdAt: "Criado em",
+  updatedAt: "Atualizado em",
+};
